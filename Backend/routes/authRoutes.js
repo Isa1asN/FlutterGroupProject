2012906cfea11c487155dc9bcc7f2f5730ca2eb8
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-05-24 00:06:43 +0300

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,28 +1,27 @@
 import express from "express";
-import {register, login, userEvent, adminEvent} from "../controllers/authController.js"
+import {register, login} from "../controllers/authController.js"
 import { verifyUserToken, IsAdmin, IsUser } from "../middleware/auth.js";
 import { forgotPassword, resetPassword } from '../controllers/forgotPassword.js';
 import { logout, deleteAccount } from "../controllers/authController.js";
 
 
-export const router = express.Router()
+export const authRouter = express.Router()
 
 // Register a new User
-router.post('/register', register);
+authRouter.post('/register', register);
 
 // Login
-router.post('/login', login);
+authRouter.post('/login', login);
 
 // Forgot Password
-router.post('/forgotPassword', forgotPassword);
+authRouter.post('/forgotPassword', forgotPassword);
 
 // Reset Password
-router.post('/resetPassword', resetPassword);
+authRouter.post('/resetPassword', resetPassword);
 
 // Logout 
-router.post('/logout', logout);
+authRouter.post('/logout', logout);
 
 // Delete user Account
-router.delete('/deleteAccount', deleteAccount);
-
+authRouter.delete('/deleteAccount', deleteAccount);
 
